c9b8b8a1823a933c825705bbdefdf613297d64c2
Niraj Dudani <niraj@locus.sh>
2017-10-21 11:18:13 +0530

    Reorganization of files, to add more projects in this repo.

--- chop.py
+++
@@ -1,6 +0,0 @@
-def chop(cropped_image):
-	import numpy as np
-	
-	imgparts = np.split(cropped_image, 10, 1)
-	
-	return imgparts
--- resize.py
+++
@@ -1,9 +0,0 @@
-def resize(images):
-	import skimage
-	
-	resized = [
-		skimage.transfom.resize(image, [24, 240])
-		for image in images
-	]
-	
-	return resized
